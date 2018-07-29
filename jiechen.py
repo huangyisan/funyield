102c628bb7b263b107a59bf88256469d087b4487
huangyisan <ahuangyisan@live.cn>
2018-07-29 17:06:58 +0800

    iadd

--- jiechen.py
+++ jiechen.py
@@ -1,5 +1,5 @@
 from functools import reduce
-from operator import mul
+from operator import mul,iadd
 def fact(n):
     return reduce(lambda x,y:x*y, range(1,n+1))
 print(fact(10))
@@ -8,3 +8,9 @@ print(fact(10))
 def factmul(n):
     return reduce(mul, range(1, n+1))
 print(factmul(10))
+
+
+
+def addadd(n):
+    return reduce(iadd, range(1,n+1))
+print(addadd(100))
