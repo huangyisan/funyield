62206824895481c2f461a0d87b41a9cec0129589
huangyisan <huangyisan@qmtv.com>
2018-08-01 19:49:32 +0800

    update

--- jsonfunc1.py
+++ jsonfunc1.py
@@ -1,17 +1,22 @@
 import json
+str_a = '{"a": 1,"b": 0,"d": "d","f": "1.1"}'
+dict_a = {'a': 1, 'b': 0, 'd': 'd', 'f': '1.1'}
+print(json.dumps(str_a), ' '*5, type(json.dumps(str_a)))
+print(json.dumps(dict_a), ' '*5, type(json.dumps(dict_a)))
 
-a = {'a': 1, 'b': 0, 'd': 'd', 'f': '1.1'}
-b = '{"a": 1, "b": 0, "d": "d", "f": "1.1"}'
+print(json.loads(str_a), ' '*5, type(json.loads(str_a)))
+# print(json.loads(dict_a), ' '*5, type(json.loads(dict_a)))
 
-# with open('test.list', 'w') as f:
-#     json.dump(a, f)
+# dumps(字典，类json字符串) --> 产出为类json字符串
+# loads(类json字符串) --> 产出为字典
 
+with open('test.list','w') as f:
+    json.dump(str_a, f)
 
-print(json.dumps(a))
+with open('test.list','r') as f:
+    data = json.load(f)
+print('*'*5)
+print(data, ' '*5, type(data))
 
-# with open('test.list', 'r') as f:
-#     result = json.load(f)
-
-print(json.loads(b))
-
-# print(result)
+# dump(类json字符串 或者 字典) ———— 将dumps的内容写入文本
+# load(类json字符串 或者 字典)从文本读出 ———— 文本内容样式为str则产出是str，文本内容样式是字典则产出是字典
