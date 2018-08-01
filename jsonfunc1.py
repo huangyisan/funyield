2d15a2fdb16ab165589c1d20f051b0ac0d401c36
huangyisan <huangyisan@qmtv.com>
2018-08-01 18:32:34 +0800

    update

--- jsonfunc1.py
+++ jsonfunc1.py
@@ -1,3 +1,17 @@
 import json
-a = {"name":"huangyisan"}
+
+a = {'a': 1, 'b': 0, 'd': 'd', 'f': '1.1'}
+b = '{"a": 1, "b": 0, "d": "d", "f": "1.1"}'
+
+# with open('test.list', 'w') as f:
+#     json.dump(a, f)
+
+
 print(json.dumps(a))
+
+# with open('test.list', 'r') as f:
+#     result = json.load(f)
+
+print(json.loads(b))
+
+# print(result)
