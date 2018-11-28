4bde0e5887b5e5d86d0e5784e14e7434b407c0db
huangyisan <anonymousyisan@gmail.com>
2018-11-28 16:37:04 +0800

    update

--- sum.py
+++ sum.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(0,len(nums)):
-            j = 1
-            while j:
-                print(nums[i])
-                if nums[i] > target:
+            while True:
+                other_num = target - nums[i]
+                try:
+                    other_num_index = nums.index(other_num)
+                except Exception:
                     break
-                if nums[i] + nums[j] == target and nums[i] != nums[j]:
-                    return [i,j]
                 else:
-                    j+=1
-                    if j==len(nums):
+                    if i == other_num_index:
                         break
-        return "cant find"
-nums = [10,2,3,4,5,1]
-target = 11
+                    return [i,other_num_index]
+nums = [3,2,4]
+target = 6
 a = Solution()
 print(a.twoSum(nums=nums, target=target))
